206a519643640a7607fe8671d6619429fabae00e
mohammed ahmed <64513301+mohammedahmed18@users.noreply.github.com>
2022-02-14 19:17:21 +0200

    Update UserItem.js

--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -1,7 +1,5 @@
 import PropTypes from "prop-types";
 import { useContext } from "react";
-import noImage from "../images/image-blank.png";
-
 import GithubContext from "../contexts/GithubContext";
 import { Link } from "react-router-dom";
 
